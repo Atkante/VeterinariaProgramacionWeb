98d5648fef03670d2ba63978d0c6a6d18dbe41fa
Atkante <anthonyor029@gmail.com>
2021-11-29 23:46:12 -0500

    Dashboard

---
+++ Admin/js/mascotas.js
@@ -0,0 +1,7 @@
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('click')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
